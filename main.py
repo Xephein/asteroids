a6ed8a84fa3cb14c6d96d961e084832404e5bd63
xephein <friesz.marton@gmail.com>
2025-05-13 23:17:28 +0200

    add player, add rotation, add circleshape, update constants

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
 
+from circleshape import *
+from player import *
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
+        #Set background color, draw on this
         screen.fill("black")
+
+        player.draw(screen)
         pygame.display.flip()
+
+        #End of the game loop
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
