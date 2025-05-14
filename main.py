fd03a0c9f751ce3d803639e329ec868871fa2798
xephein <friesz.marton@gmail.com>
2025-05-14 13:45:52 +0200

    add movement, add asteroid, add asteroidfield

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import pygame
 
+from asteroid import *
+from asteroidfield import *
 from circleshape import *
-from player import *
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    group_updatable = pygame.sprite.Group()
+    group_drawable = pygame.sprite.Group()
+    group_asteroids = pygame.sprite.Group()
+
+    Player.containers = (group_drawable, group_updatable)
+    Asteroid.containers = (
+        group_asteroids, 
+        group_drawable, 
+        group_updatable
+    )
+    AsteroidField.containers = (group_updatable)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
@@ -15,11 +31,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        group_updatable.update(dt)
         #Set background color, draw on this
         screen.fill("black")
 
-        player.draw(screen)
+        for drawable in group_drawable:
+            drawable.draw(screen)
         pygame.display.flip()
 
         #End of the game loop
